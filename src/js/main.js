d14277e8a72374a46a115ab2f9d6bfc899d3a50c
vadimbelinovich <vadimbelinovich@gmail.com>
2019-12-03 16:33:48 +0300

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,6 @@ import scroll from "./component/scroll";
 import docSlider from "./component/slider";
 import tabIndex from "./component/tabs";
 
-if(window.innerWidth < 992) {
+if(window.innerWidth < 992 && document.querySelector('.slider')) {
   document.querySelector('.slick-dots').style.top = document.querySelector('.slider .pic-1').offsetHeight + 10 + "px";
 }
