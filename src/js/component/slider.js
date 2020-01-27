fd3a037e1561949de292389832e306a8ead3dfca
vadimbelinovich <vadimbelinovich@gmail.com>
2020-01-27 16:23:33 +0300

    edited

--- src/js/component/slider.js
+++ src/js/component/slider.js
@@ -1,5 +1,5 @@
 function docSlider() {
-  if($('.doctors ul.slider').length) {
+  if ($('.doctors ul.slider').length) {
     $('.doctors ul.slider').slick({
       autoplay: false,
       autoplaySpeed: 6000,
@@ -15,8 +15,24 @@ function docSlider() {
     });
   }
 
-  if($('ul.review__list').length) {
+  if ($('ul.review__list').length) {
     $('ul.review__list').slick({
+      autoplay: false,
+      autoplaySpeed: 6000,
+      dots: true,
+      arrows: false,
+      mobileFirst: true,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: 'unslick'
+        }
+      ]
+    });
+  }
+
+  if ($('main.team ul.list').length) {
+    $('main.team ul.list').slick({
       autoplay: false,
       autoplaySpeed: 6000,
       dots: true,
